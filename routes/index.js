dc434d8267f6419c86cf4fd1b9cc86067dc8c0c8
RobbieClarken <rob.clarken@gmail.com>
2012-08-27 19:20:40 +1000

    Load admin from routes/index

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'English Auction' });
 };
+
+exports.admin = function(req, res){
+  res.render('admin', { title: 'English Auction Admin' });
+};
